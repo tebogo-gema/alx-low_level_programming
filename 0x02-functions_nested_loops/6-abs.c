d7727bb200b52c63242af1b9b5c692fd01f7ef0a
tebogo-gema <gematebogo@gmail.com>
2023-06-20 11:24:41 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,14 +2,14 @@
 
 /**
  * _abs - computes the absolute value of an integer
- * @n: the int to check
+ * @i: the int to check
  * Return: the absolute value of int
  */
 int _abs(int i)
 {
 	if (i >= 0)
 	{
-		return (n);
+		return (i);
 	}
 	else
 	{
